ce9c0dc11c3d179be0f7494079abd37f89010a60
WackThat <martin50679@gmail.com>
2017-11-24 16:57:21 +0200

    Search History Route Update

--- app.js
+++ app.js
@@ -4,30 +4,46 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const GoogleImages = require('google-images');
 const client = new GoogleImages('007733815140463231320:i_fnlkxqon0', process.env.API_KEY);
-// "49EB4B94127F7C7836C96DEB3F2CD8A6D12BDB71"
-// Google API Key "AIzaSyA7pWcsUjrkQSDHQNukXJH79djUuAGiEHo"
 // Connection To The Database
 mongoose.connect('mongodb://localhost/imagesearch');
+const db = mongoose.connection;
 // Express Middleware
 const app = express();
 // BodyParser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+// Logs Database Schema
+const LogSchmea = mongoose.Schema({
+    search: String,
+    date: String
+});
+// Compiling The Schema Into A Model
+const Logs = mongoose.model('Logs', LogSchmea);
 // Search Proccess
-app.get('/search/imagesearch/:searchTerm/:offset?', (req, res, next) => {
+app.get('/imagesearch/:searchTerm/:offset?', (req, res, next) => {
     var searchTerm = req.params.searchTerm;
     var offset = req.params.offset;
     (offset == undefined) ? offset = 1 : offset ;
     // Search With The Google API
     client.search(searchTerm, {page: offset})
     .then(images => {
+        var d = new Date();
+        let time = d.toLocaleDateString() + " " + d.toLocaleTimeString(); 
         let display = [];
+        let log = new Logs({"search": searchTerm, "date": time});
+        db.collection('recentSearches').insert(log);
         for(var i=0;i<images.length;i++){
             display.push({"url": images[i].url, "snippet": images[i].description, "thumbnail": images[i].thumbnail, "context": images[i].parentPage});
         }
         res.json(display);
     });
+});
+// Search History
+app.get('/recent', (req, res) => {
+    db.collection('recentSearches').find({}).toArray((err, doc) => {
+        res.json(doc.reverse());
     });
+});
 // Server Start
 app.listen(3000, () => {
     console.log('Server is on.');
